d682be611491ca1ec0922e257eccd78eb48b3e36
Mustafa - Linux <mustafaazeem@hotmail.com>
2025-07-30 14:47:20 +0200

    A

--- config_flow.py
+++ config_flow.py
@@ -14,6 +14,26 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    '''Sets up a_smart_sim from a config entry.'''
+    _LOGGER.info("Setting up a_smart_Sim integration with entry %s", entry.title)
+
+    # Store config entry data. 
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data 
+
+    # Forward the stored data to be setup by the sensor platform.
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    '''Unload a config entry specified in entry variable.'''
+    _LOGGER.info("Unload a_smart_sim config entry with id %s", entry.title)
+
+    # Unload platform.
+    if unload.ok := await hass.config_entries.async_unload_platform(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
 
 class ASmartSimConfigFlow(ConfigFlow, domain=DOMAIN):
     """Create a configuration flow for a_smart_sim."""
