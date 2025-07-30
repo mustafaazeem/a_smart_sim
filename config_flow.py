1550655039d8277233a2f0955985dcb8f898a10b
Mustafa Hassan <mustafaazeem@hotmail.com>
2025-07-30 14:30:07 +0200

    m

--- config_flow.py
+++ config_flow.py
@@ -1,4 +1,5 @@
-'''Config Flow for a_smat_sim.'''
+"""Config Flow for a_smat_sim."""
+
 from __future__ import annotations
 
 import logging
@@ -7,24 +8,23 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow
-
+from homeassistant.const import Platform
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
 class ASmartSimConfigFlow(ConfigFlow, domain=DOMAIN):
-    '''Create a configuration flow for a_smart_sim.'''
+    """Create a configuration flow for a_smart_sim."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        '''Handle the initial setup via user-step.'''
-        
-        if user_input is None:
-            '''If user just visited first time, there is no input, send default form then.'''
-            return self.async_show_form(step_id='user')
-        
-        return self.async_create_entry(title= 'a_smart_sim', data=user_input)
-        
+        """Handle the initial setup via user-step."""
 
+        if user_input is None:
+            """If user just visited first time, there is no input, send default form then."""
+            return self.async_show_form(step_id="user")
+
+        return self.async_create_entry(title="a_smart_sim", data=user_input)
