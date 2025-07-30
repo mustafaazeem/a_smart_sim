ed3eb694dd47d72fb2a15462afbc18f9041af7ba
Mustafa - Linux <mustafaazeem@hotmail.com>
2025-07-30 15:39:18 +0200

    B

--- config_flow.py
+++ config_flow.py
@@ -7,33 +7,20 @@ from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow
-from homeassistant.const import Platform
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-PLATFORMS: list[Platform] = [Platform.SENSOR]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    '''Sets up a_smart_sim from a config entry.'''
-    _LOGGER.info("Setting up a_smart_Sim integration with entry %s", entry.title)
+STEP_USER_DATA_SCHEMA = vol.schema (
+    {
+        vol.Required("name", default = "Mustafa"): str,
+        vol.Required("update_interval", default = 10): int
+    }
+)
 
-    # Store config entry data. 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data 
-
-    # Forward the stored data to be setup by the sensor platform.
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    '''Unload a config entry specified in entry variable.'''
-    _LOGGER.info("Unload a_smart_sim config entry with id %s", entry.title)
-
-    # Unload platform.
-    if unload.ok := await hass.config_entries.async_unload_platform(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
 
 class ASmartSimConfigFlow(ConfigFlow, domain=DOMAIN):
     """Create a configuration flow for a_smart_sim."""
